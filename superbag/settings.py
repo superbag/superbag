fdbe9eb9b88b97035bf16b0c1b43aed969ed78ba
Viktor.C <ivan.tikhonov@kuehne-nagel.com>
2016-03-13 18:48:09 +0200

    db fix

--- superbag/settings.py
+++ superbag/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'superbag',
         'USER': 'django',
-        'PASSWORD': 'ABMfn1qu57',
+        'PASSWORD': 'VhlrFQ12y4',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'DEFAULT_CHARSET': 'utf-8'
